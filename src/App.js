9f36be1db2fdebccc0325412a7c1618c243296d6
Lavkush Maurya <113632168+lavkush-maurya@users.noreply.github.com>
2023-05-21 17:39:07 +0530

    Update App.js
    updating routes

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ const App = () => {
       <div className="App" style={{ textAlign: "-webkit-center" }}>
         <h1>Crud redux API</h1>
         <div style={{ marginBottom: "20px" }}>
-          <Link to={"/"}>HOme</Link>
+          <Link to={"/crud-redux-api"}>HOme</Link>
           <Link to={"/insert"}>Insert</Link>
         </div>
         <Routes>
-          <Route path="/" element={<Apidatadisplay />} />
+          <Route path="/crud-redux-api" element={<Apidatadisplay />} />
           <Route path="/insert" element={<Insertpage />} />
           <Route path="/edit/:id" element={<Editpage />} />
         </Routes>
